b691f4a65801c7d201ac1155a05747f50f27c524
quentin <emo.quentin@gmail.com>
2021-04-01 13:25:41 +0200

    first commit

---
+++ src/test/java/fr/quentin/springframework/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.quentin.springframework.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
