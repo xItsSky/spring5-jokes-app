d8046ca48b50bc404659533f5bfb809f60077eb3
quentin <emo.quentin@gmail.com>
2021-04-01 14:00:00 +0200

    jokes

---
+++ src/main/java/fr/quentin/springframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package fr.quentin.springframework.spring5jokesapp.controllers;
+
+import fr.quentin.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by quentin on 01/04/2021
+ **/
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
+}
---
+++ src/main/java/fr/quentin/springframework/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,8 @@
+package fr.quentin.springframework.spring5jokesapp.services;
+
+/**
+ * Created by quentin on 01/04/2021
+ **/
+public interface JokeService {
+    String getJoke();
+}
